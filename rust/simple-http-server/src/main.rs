f4a95562941e6d084929365d812629ed03004bba
clxxiii <clxxiii@users.noreply.github.com>
2023-04-01 02:45:00 -0400

    add simple server example (which doesn't work)

---
+++ rust/simple-http-server/src/main.rs
@@ -0,0 +1,11 @@
+extern crate simple_server;
+
+use simple_server::Server;
+
+fn main() {
+    let server = Server::new(|_request, mut response| {
+        Ok(response.body("Hello, world!".as_bytes().to_vec())?)
+    });
+
+    server.listen("127.0.0.1", "7979");
+}
