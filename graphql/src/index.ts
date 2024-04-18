1e435d3053b5aef720c905c3fe3eab751f0074c1
clxxiii <clxxiii@users.noreply.github.com>
2024-04-18 01:31:22 -0400

    way too many comments

--- graphql/src/index.ts
+++ graphql/src/index.ts
@@ -1,27 +1,31 @@
+/**
+ * This file loads the web frontend, which allows you to interact with the repo's
+ * GraphQL API.
+ */
+
 import express from "express";
-// import { createHandler } from "graphql-http/lib/use/express";
 import { ruruHTML } from "ruru/server"
 import { readFileSync } from "fs";
 import { graphqlHTTP } from "express-graphql";
-// import { buildSchema } from "graphql";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import resolvers from "./resolvers"
 
-const typeDefs = (readFileSync("./src/schema.gql").toString());
+const typeDefs = (readFileSync("./src/schema.gql").toString()); // Pull Schema from schema file
 
 const schema = makeExecutableSchema({
   typeDefs,
-  resolvers
+  resolvers // Pull Functions from "resolvers" file
 })
+
 
 const app = express();
 app.all(
-  "/graphql",
+  "/graphql", // Create a GraphQL endpoint at /graphql
   graphqlHTTP({
     schema,
   })
 ).get(
-  "/", (_req, res) => {
+  "/", (_req, res) => { // Create the frontend page.
     res.type("html");
     res.end(ruruHTML({ endpoint: "/graphql" }))
   }
--- graphql/src/resolvers.ts
+++ graphql/src/resolvers.ts
@@ -1,8 +1,16 @@
 import { PrismaClient, type Like, type Post, type User } from "@prisma/client";
 
+// Prisma is the ORM I am using to simplify the SQL queries.
+// Could be replaced with raw SQL, but input would still need
+// to be sanitized to prevent SQL injections.
 const prisma = new PrismaClient();
 
 export default {
+  /**
+   * QUERIES:
+   * Queries are reads on your API. Each function below
+   * is treated as a "starter", for where to enter the graph.
+   */
   Query: {
     hello: () => "Hi!",
 
@@ -19,21 +27,26 @@ export default {
     }
   },
 
+  /**
+   * Once you're in the graph, we need to specify how to get from
+   * one table to the other.
+   */
   User: {
     posts: (user: User) => (prisma.post.findMany({ where: { author_id: user.id } })),
     likes: (user: User) => (prisma.post.findMany({ where: { likes: { some: { user_id: user.id } } } }))
   },
-
   Post: {
     author: (post: Post) => prisma.user.findUnique({ where: { id: post.author_id } }),
     likes: (post: Post) => prisma.user.findMany({ where: { likes: { some: { post_id: post.id } } } })
   },
-
   Like: {
     author: (like: Like) => prisma.user.findUnique({ where: { id: like.user_id } }),
     post: (like: Like) => prisma.post.findUnique({ where: { id: like.post_id } })
   },
 
+  /**
+   * Here is where the mutations are specified.
+   */
   Mutation: {
     makePost: (_: undefined, params: { content: string, username: string }) => (
       prisma.post.create({
